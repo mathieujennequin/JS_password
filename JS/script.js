563d2a71b651e45e55156ad78b13388199b48eb1
unknown <mjennequin.simplon@gmail.com>
2018-08-07 12:30:40 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+var pseudo = prompt("Entrez votre pseudo:");
+var password = prompt("Entrez votre mot de passe:");
+
+if (pseudo === "mathieu" && password === "hello") {
+    alert('Connexion réussie !');
+}
+else {
+    alert('Erreur, veuillez recommencer');
+}
